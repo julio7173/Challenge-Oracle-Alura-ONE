e0b1d998fb75efc044e68a81ea248004ff440d63
julio7173 <juliocesarsevericheorellana@gmail.com>
2023-05-24 20:58:54 -0400

    Modificación de criptografia.html y criptografia.css, además de agregar criptografia.js

---
+++ criptografia.js
@@ -0,0 +1,11 @@
+function mostrarRedes(){
+    var nave = document.getElementById("nave");
+    var instagram = document.getElementById("instagram");
+    var linkedin = document.getElementById("linkedin");
+    var github = document.getElementById("github");
+
+    nave.classList.toggle("clicked");
+    instagram.classList.toggle("clicked");
+    linkedin.classList.toggle("clicked");
+    github.classList.toggle("clicked");
+}
