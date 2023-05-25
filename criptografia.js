9c29500995ad97028c9b9cdb9182b90e13e060e2
julio7173 <juliocesarsevericheorellana@gmail.com>
2023-05-24 22:25:39 -0400

    Modificando errores en criptografia.html y añadiendo funcionalidades a criptografia.js

--- criptografia.js
+++ criptografia.js
@@ -9,3 +9,55 @@ function mostrarRedes(){
     linkedin.classList.toggle("clicked");
     github.classList.toggle("clicked");
 }
+
+var astronauta = document.getElementById("astronauta");
+var alien = document.getElementById("alien");
+var encriptar = document.getElementById("encriptar");
+var desencriptar = document.getElementById("desencriptar");
+var reglasEncriptar = {"e":"enter", "i":"imes", "a":"ai", "o":"ober", "u":"ufat"};
+var reglasDesencriptar = {"enter":"e", "imes":"i", "ai":"a", "ober":"o", "ufat":"u"};
+
+function encriptarTexto(){
+    var texto = astronauta.value;
+    for(var vocal in reglasEncriptar){
+        var palabra = reglasEncriptar[vocal];
+        var regex = new RegExp(vocal, "i");
+        texto = texto.replace(regex, palabra);
+    }
+    alien.value = texto;
+}
+
+function desencriptarTexto(){
+    var texto = astronauta.value;
+    for(var vocal in reglasDesencriptar){
+        var palabra = reglasDesencriptar[vocal];
+        var regex = new RegExp(vocal, "gi");
+        texto = texto.replace(regex, palabra);
+    }
+    alien.value = texto;
+}
+
+encriptar.addEventListener("click", encriptarTexto);
+desencriptar.addEventListener("click", desencriptarTexto);
+
+async function copiarTexto(){
+    var texto = document.getElementById("alien").value;
+    try{
+        await navigator.clipboard.writeText(texto);
+        alert("Copiado!");
+    }
+    catch(err){
+        alert("Error al copiar" + err);
+    }
+}
+
+var copiar = document.getElementById("copiar");
+copiar.addEventListener("click", copiarTexto);
+
+function borrarTexto(){
+    astronauta.value = "";
+    alien.value = "";
+}
+
+var borrar = document.getElementById("borrar");
+borrar.addEventListener("click", borrarTexto);
